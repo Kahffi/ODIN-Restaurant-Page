56aff25216a2af10c8111bedc8c3508ae02efd65
kahffi <daffaalkahffi24@gmail.com>
2024-07-15 23:45:35 +0700

    setting up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
+const { watch, watchFile } = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+	mode: "development",
 	entry: "./src/index.js",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	devServer: {
+		static: "./dist",
+		watchFiles: ["./src/index.html"],
+	},
+	devtool: "inline-source-map",
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html",
+			inject: "body",
+		}),
+	],
 };
